c66444782a83f6dba2f7c8eff25919a0f5de62db
Mad <madbessoul@gmail.com>
2019-07-06 22:24:21 +0200

    Added hello world from Heroku rest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private http: HttpClient) {}
+
+  message;
   title = 'heroku-app';
+
+  ngOnInit()  {
+    this.http.get('https://heroku-rest-0.herokuapp.com/').subscribe(data => {
+      console.log(data);
+      this.message = data;
+    }
+
+    )
+  }
+
 }
