1d1b64e8795cb43b7f37341c2f15419dea816e34
Mad <madbessoul@gmail.com>
2019-07-07 12:55:30 +0200

    Used async in template for greeting, removed spec files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 
 export interface Greeting {
@@ -18,14 +19,11 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  message: Greeting;
+  message$: Observable<Greeting>;
   title = 'heroku-app';
 
   ngOnInit()  {
-    this.http.get<Greeting>('https://heroku-rest-0.herokuapp.com/greeting?name=Mad').subscribe(data => {
-      
-      this.message = data;
-    });
+    this.message$ = this.http.get<Greeting>('https://heroku-rest-0.herokuapp.com/greeting?name=Mad');
   }
 
 }
